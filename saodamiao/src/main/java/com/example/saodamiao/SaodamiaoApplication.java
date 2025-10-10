bfca924ac61febcaad2fe23d6216ed0d150af412
LuisDev <luisnantes123@gmail.com>
2025-10-10 20:23:16 -0300

    Projeto iniciado: Estrutura de pastas criadas e aprovadas

---
+++ saodamiao/src/main/java/com/example/saodamiao/SaodamiaoApplication.java
@@ -0,0 +1,14 @@
+package com.example.saodamiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaodamiaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaodamiaoApplication.class, args);
+        System.out.println("SaodamiaoApplication started");
+    }
+
+}
---
+++ saodamiao/src/test/java/com/example/saodamiao/SaodamiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saodamiao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaodamiaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
