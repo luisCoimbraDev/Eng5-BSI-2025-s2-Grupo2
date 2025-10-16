e0a5f09c7b97824a27231c97136ed1c2ae110df9
LuisDev <luisnantes123@gmail.com>
2025-10-16 11:20:50 -0300

    fix: Retornos da função de tipo de Alimento arrumado

--- saodamiao/src/main/java/com/example/saodamiao/Control/TipoAlimentoControl.java
+++ saodamiao/src/main/java/com/example/saodamiao/Control/TipoAlimentoControl.java
@@ -5,14 +5,13 @@ import com.example.saodamiao.DTO.TipoAlimentoDTO;
 import com.example.saodamiao.Model.TipoAlimento;
 import com.example.saodamiao.Singleton.Erro;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "apis/tipoalimento")
 public class TipoAlimentoControl {
+
     @PostMapping(value = "/inserir")
     public ResponseEntity<Object> InsereTipoAlimento(@RequestBody TipoAlimentoDTO tipoAlimento){
         TipoAlimento t1 = new TipoAlimento();
