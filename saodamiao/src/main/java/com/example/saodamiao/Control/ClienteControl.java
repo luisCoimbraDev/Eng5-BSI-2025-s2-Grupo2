18daec0054b3427e2ca08be3fdf8c3029f2a7874
Luis <elekrsilva@gmail.com>
2025-10-16 17:41:32 -0300

    feat - "Adicionado as classes ClienteDAO(Implementada) e ClienteControl falta métodos"

---
+++ saodamiao/src/main/java/com/example/saodamiao/Control/ClienteControl.java
@@ -0,0 +1,20 @@
+package com.example.saodamiao.Control;
+
+import com.example.saodamiao.Model.Alimento;
+import com.example.saodamiao.Model.Cliente;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value="apis/clientes")
+public class ClienteControl {
+
+    @PostMapping(value = "/inserir")
+    public ResponseEntity<Object> InsereAlimento(@RequestBody Cliente cliente){
+        if(cliente.getClienteDAO().gravar(cliente))
+            return ResponseEntity.ok(cliente);
+        return ResponseEntity.badRequest().body("Falha ao inserir o Cliente!!");
+    }
+
+}
---
+++ saodamiao/src/main/java/com/example/saodamiao/DAO/ClienteDAO.java
@@ -0,0 +1,91 @@
+package com.example.saodamiao.DAO;
+
+import com.example.saodamiao.Model.Cliente;
+import com.example.saodamiao.Singleton.Singleton;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteDAO implements IDAO<Cliente> {
+
+    @Override
+    public boolean gravar(Cliente entidade) {
+        String sql = """
+                INSERT INTO CLIENTE(IDCLIENTE, NOME, CPF, TELEFONE, EMAIL)
+                VALUES(NEXTVAL('SEQ_CLIENTE'), '#2', '#3', '#4', '#5');
+                """;
+        sql = sql.replace("#2", entidade.getNome());
+        sql = sql.replace("#3", entidade.getCpf());
+        sql = sql.replace("#4", entidade.getTelefone());
+        sql = sql.replace("#5", entidade.getEmail());
+        return Singleton.Retorna().manipular(sql);
+    }
+
+    @Override
+    public boolean alterar(Cliente entidade, int id) {
+        String sql = """
+                UPDATE CLIENTE
+                SET NOME = '#1',
+                    CPF = '#2',
+                    TELEFONE = '#3',
+                    EMAIL = '#4' WHERE IDCLIENTE = #5;
+                """;
+        sql = sql.replace("#1", entidade.getNome());
+        sql = sql.replace("#2", entidade.getCpf());
+        sql = sql.replace("#3", entidade.getTelefone());
+        sql = sql.replace("#4", entidade.getEmail());
+        sql = sql.replace("#5", "" + id);
+        return Singleton.Retorna().manipular(sql);
+    }
+
+    public Cliente PegarCliente(String CPF) {
+        Cliente cliente = null;
+        String sql = "SELECT * FROM CLIENTE WHERE CPF = '" + CPF + "'";
+        ResultSet rs = Singleton.Retorna().consultar(sql);
+
+        try {
+            if (rs.next()) {
+                cliente = new Cliente(
+                        rs.getString("NOME"),
+                        rs.getString("CPF"),
+                        rs.getString("TELEFONE"),
+                        rs.getString("EMAIL"),
+                        rs.getInt("IDCLIENTE")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return cliente;
+    }
+
+    @Override
+    public boolean apagar(Cliente entidade) {
+        return Singleton.Retorna().manipular("DELETE FROM CLIENTE WHERE CPF = '" + entidade.getCpf() + "'");
+    }
+
+    @Override
+    public List<Cliente> pegarListaToda() {
+        List<Cliente> clientes = new ArrayList<>();
+        String sql = "SELECT * FROM CLIENTE";
+        ResultSet rs = Singleton.Retorna().consultar(sql);
+        try {
+            while (rs.next())
+            {
+                clientes.add(new Cliente(
+                        rs.getString("NOME"),
+                        rs.getString("CPF"),
+                        rs.getString("TELEFONE"),
+                        rs.getString("EMAIL"),
+                        rs.getInt("IDCLIENTE")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return clientes;
+    }
+}
