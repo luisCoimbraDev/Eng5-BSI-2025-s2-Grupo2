a8de547145a2ccc4b1a26631a961109f10e8f3ee
LuisDev <luisnantes123@gmail.com>
2025-11-07 14:48:15 -0300

    Versao do felipe funcionando

--- saodamiao/src/main/java/com/example/saodamiao/Control/InspecaoControl.java
+++ saodamiao/src/main/java/com/example/saodamiao/Control/InspecaoControl.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RequestMapping("/apis/inspecao")
 public class InspecaoControl {
 
